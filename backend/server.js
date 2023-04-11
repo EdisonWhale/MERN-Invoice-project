c584a6d7490f7e669457497ed1483ff8d644bc8a
EdisonWhale <edisonwhalex@gmail.com>
2023-04-10 22:02:39 -0400

    email helper

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import connectionToDB from "./config/connectDB.js";
 import { morganMiddleware, systemLogs } from "./utils/Logger.js";
+import mongoSanitize from "express-mongo-sanitize";
+await connectionToDB();
 
 const app = express();
 
@@ -17,6 +20,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use(cookieParser());
+
+app.use(mongoSanitize());
 
 app.use(morganMiddleware);
 
@@ -36,14 +41,3 @@ app.listen(PORT, () => {
 		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
 	);
 });
-
-process.on("uncaughtException", (err) => {
-	console.error("Uncaught Exception:", err);
-	process.exit(1);
-  });
-  
-  process.on("unhandledRejection", (reason, promise) => {
-	console.error("Unhandled Rejection at:", promise, "reason:", reason);
-	process.exit(1);
-  });
-  
