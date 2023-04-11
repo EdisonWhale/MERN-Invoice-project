d0a7f0c14827da96572e97586a5df1f9deadc9e9
EdisonWhale <edisonwhalex@gmail.com>
2023-04-11 16:41:38 -0400

    feat:deactive user account and deleted user accoun

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,8 +4,9 @@ import checkAuth from "../middleware/checkAuthMiddleware.js";
 import updateUserProfile from "../controllers/user/updateUserProfile.js";
 import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
 import getAllUserAccounts from "../controllers/user/getAllUserAccounts.js";
+import deleteUserAccount from "../controllers/user/deleteUserAccount.js";
+import deactivateUser from "../controllers/user/deactivateUser.js";
 import role from "../middleware/roleMiddleware.js";
-
 
 const router = express.Router();
 
@@ -14,10 +15,16 @@ router
 	.get(checkAuth, getUserProfile)
 	.patch(checkAuth, updateUserProfile)
 	.delete(checkAuth, deleteMyAccount);
-	
+
 router
 	.route("/all")
 	.get(checkAuth, role.checkRole(role.ROLES.Admin), getAllUserAccounts);
 
-	
+router
+	.route("/:id")
+	.delete(checkAuth, role.checkRole(role.ROLES.Admin), deleteUserAccount);
+
+router
+	.route("/:id/deactivate")
+	.patch(checkAuth, role.checkRole(role.ROLES.Admin), deactivateUser);
 export default router;
