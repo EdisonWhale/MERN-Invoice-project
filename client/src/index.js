8f5f58d8d38c9de4185c23dfae0a83726385f07a
EdisonWhale <edisonwhalex@gmail.com>
2023-04-11 17:40:53 -0400

    feat:add ui and hooks and react compents

--- client/src/index.js
+++ client/src/index.js
@@ -6,18 +6,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<Routes>
+					<Route path="/*" element={<App />} />
+				</Routes>
+			</Router>
+		</Provider>
+	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
