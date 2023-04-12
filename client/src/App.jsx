199b0049c83637fa32cacf72537e01b9df9cdd94
EdisonWhale <edisonwhalex@gmail.com>
2023-04-12 01:44:36 -0400

    feat: add protect route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,12 @@ import LoginPage from "./features/auth/pages/LoginPage";
 import ResendEmailTokenPage from "./features/auth/pages/ResendEmailTokenPage";
 import PasswordResetPage from "./features/auth/pages/PasswordResetPage";
 import PasswordResetRequestPage from "./features/auth/pages/PasswordResetRequestPage";
+
+import { ROLES } from "./config/role";
+import UsersList from "./features/users/pages/UsersListPage"
+import DashboardPage from "./pages/DashboardPage";
+import AuthRequired from "./components/AuthRequired";
+
 const App = () => {
   useTitle("MERN Invoice - Home");
 	const { user } = useSelector((state) => state.auth);
@@ -39,6 +45,20 @@ const App = () => {
 						path="auth/reset_password"
 						element={<PasswordResetPage />}
 					/>
+
+          {/* Private Routes - Users */}
+					<Route
+						element={<AuthRequired allowedRoles={[ROLES.User]} />}
+					>
+            <Route path="dashboard" element={<DashboardPage/>}/>
+          </Route>
+
+					{/* Private Routes - Admin Users only */}
+					<Route
+						element={<AuthRequired allowedRoles={[ROLES.Admin]} />}
+					>
+						<Route path="users" element={<UsersList />} />
+					</Route>
           
           <Route path="*" element={<NotFound />} />
         </Route>
