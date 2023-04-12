816bf2407194aa049ad1e6fd180d6ba34af4add3
EdisonWhale <edisonwhalex@gmail.com>
2023-04-12 01:44:36 -0400

    feat: add protect route

---
+++ client/src/features/users/pages/UsersListPage.jsx
@@ -0,0 +1,20 @@
+import{Box,Typography} from "@mui/material"
+
+const UserListPage =() =>{
+    return(
+        <Box
+        sx={{
+            display: "flex",
+            marginLeft: "250px",
+            mt: 10
+        }}
+        >
+            <Typography variant="h3"> This is Admin page
+            </Typography>
+
+
+        </Box>
+    );
+};
+
+export default UserListPage;
